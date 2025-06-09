0ddd26756ec94a366e1b9b93eeb30965bcff70b7
AnatoliyShevchenko <genitalgrinder90@outlook.com>
2025-06-09 21:10:42 +0500

    add to admin

--- chats/admin.py
+++ chats/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from chats.models import Chat, Message
+
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    model = Chat
+    list_display = ("title", "is_group")
+    list_filter = ("title",)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    model = Message
+    list_display = ("sent_at", "chat")
+    list_filter = ("sent_at",)
