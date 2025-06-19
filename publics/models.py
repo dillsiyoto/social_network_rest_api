290a716f954ebba7d27a99164549aae2505e4b81
AnatoliyShevchenko <genitalgrinder90@outlook.com>
2025-06-19 21:06:04 +0500

    join publics migrations

--- publics/models.py
+++ publics/models.py
@@ -35,3 +35,41 @@ class Public(models.Model):
 
     def __str__(self):
         return f"{self.owner} | {self.title} | {self.is_private}"
+
+
+class PublicInvite(models.Model):
+    public = models.ForeignKey(
+        to=Public, 
+        on_delete=models.CASCADE,
+        verbose_name="паблик",
+        related_name="public_invite"
+    )
+    invited_user = models.ForeignKey(
+        to=User, 
+        on_delete=models.CASCADE,
+        verbose_name="пользователь",
+        related_name="user_invited"
+    )
+    invited_by = models.ForeignKey(
+        to=User, 
+        on_delete=models.CASCADE, 
+        related_name="sent_invites",
+        verbose_name="кем приглашен"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="дата приглашения"
+    )
+    accepted = models.BooleanField(
+        default=False,
+        verbose_name="принято"
+    )
+
+    class Meta:
+        ordering = ("created_at",)
+        verbose_name = "приглашение в паблик"
+        verbose_name_plural = "приглашения в паблики"
+
+    def __str__(self):
+        return (f"{self.public.title} | {self.invited_user.username}" 
+            f" | {self.created_at} | {self.accepted}")
