dc93e65e04a59503c2dbf78da57b4f21767fa031
Adilya Amanbay <adilya.amanbay@gmail.com>
2025-06-20 21:26:49 +0500

    added chats

--- settings/urls.py
+++ settings/urls.py
@@ -7,3 +7,46 @@ urlpatterns = [
     path("api/", include("users.urls")),
     path('api/images/', include('images.urls')),
 ]
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from users.views import RegistrationViewSet, UserViewSet
+from chats.views import ChatsViewSet, MessagesViewSet
+
+router = DefaultRouter()
+router.register("registration", RegistrationViewSet, basename="registration")
+router.register("users", UserViewSet, basename="users")
+router.register("chats", ChatsViewSet, basename="chats")
+router.register("messages", MessagesViewSet, basename="messages")
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version="v1",
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/", include("users.urls")),           
+    path("api/images/", include("images.urls")),      
+    path("api/v1/", include(router.urls)),              
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+]
